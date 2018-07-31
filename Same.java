969e752a158c619a842832d191010b156e1ddcd5
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:34:07 +0000

    Create Same.java

---
+++ Same.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Same
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.nextLine();
+        System.out.print(a);
+}
+}
+
+
+
+
+
+
