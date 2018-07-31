3b2090da30be8fe513d5e5bc681bbe02b51e3a7a
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:42:09 +0000

    Create seve.java

---
+++ seve.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class seve
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+            if(n%7==0)
+            {
+                System.out.print("yes");
+            }
+            else
+            System.out.print("no");
+}
+}
+
+
+
+
+
+
