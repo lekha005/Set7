7d264eb3dd39c1d30a61c481c6989f48ef9c92fa
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:44:27 +0000

    Create diff.java

---
+++ diff.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class diff
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        int s=Math.abs(n-m);
+            if(s%2==0)
+            {
+                System.out.print("even");
+            }
+            else
+            System.out.print("odd");
+}
+}
+
+
+
+
+
+
