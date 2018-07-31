93b336ca1fbb1c077ceef926b42d6b26ea90ff57
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:18:17 +0000

    Create Subs.java

---
+++ Subs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Subs
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        int k=in.nextInt();
+        for(int i=0;i<k;i++)
+        {
+        System.out.print(a.charAt(i));
+        }
+}
+}
+
+
+
+
+
+
