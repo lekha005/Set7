f049643226fb47eb1765b73a6e7f5376e88470f7
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:47:21 +0000

    Create Two.java

---
+++ Two.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Two
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+            System.out.print(n*2);
+}
+}
+
+
+
+
+
+
