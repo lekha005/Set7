500274b1e71e22bc948b1e45e61223e2bf03e0a4
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:31:51 +0000

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Even
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+     if((a+b)%2==0)
+        System.out.print("even");
+        else
+                System.out.print("odd");
+
+}
+}
+
+
+
+
+
+
