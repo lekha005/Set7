939ec119aeccaba7dd9061857448fca9f01fabf5
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:39:59 +0000

    Create ten.java

---
+++ ten.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class ten
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=n;i<n+10;i++)
+        {
+            if(i%10==0)
+            {
+                System.out.print(i);
+            }
+        }
+
+}
+}
+
+
+
+
+
+
